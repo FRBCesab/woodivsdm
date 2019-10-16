7dc3bca2a43af7c80dc2dc2213935be60959a442
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-10-16 09:56:09 +0200

    Check if Chelsa Download is Required

--- analysis/downloadChelsa.R
+++ analysis/downloadChelsa.R
@@ -18,10 +18,37 @@ source(file.path("R", "listChelsaCmip5.R"))
 source(file.path("R", "wgetChelsa.R"))
 
 
+#' ---------------------------------------------------------------------------- @Parameters
+
+horizon  <- "1979-2013"
+
+vars     <- 1:19
+varnames <- paste0("0", vars)
+varnames <- substr(varnames, nchar(varnames) - 1, nchar(varnames))
+
+
+
+#' ---------------------------------------------------------------------------- @Check
+
+rasters <- list.files(
+  path     = file.path(path_climate_data, horizon),
+  pattern  = "\\.tif$"
+)
+
+rasters <- gsub("CHELSA_bio10_|\\.tif", "", rasters)
+
+varnames <- varnames[-which(varnames %in% rasters)]
+
+if (length(varnames) > 0) {
+
+  vars <- as.numeric(varnames)
+
+
 #' ---------------------------------------------------------------------------- @DownloadChelsaData
 
-wget_chelsa(
-  vars      = 1:19,
-  horizons  = "1979-2013",
-  path      = path_climate_data
-)
+  wget_chelsa(
+    vars      = vars,
+    horizons  = horizon,
+    path      = path_climate_data
+  )
+}
